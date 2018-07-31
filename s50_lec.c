715ce3e4f6c59f84b35cab0adb2bd6887a8c771e
mkirwa <mahlonkibiwott@gmail.com>
2018-07-30 21:29:21 -0500

    Add files via upload

--- s50_lec.c
+++ s50_lec.c
@@ -27,3 +27,23 @@ int main(void){
 
     }
 }
+
+//Programing changing lowercases to upper cases
+
+int main(void){
+
+   string s = get_string("enter string: ");
+
+
+   for(int i=0; i<strlen(s); i++){
+
+      if(s[i]>='a'&& s[i]<='z'){
+       printf("%c",s[i]-('a'-'A'));
+      }
+      else{
+         printf("%c",s[i]);
+      }
+
+    }
+    printf("\n");
+}
