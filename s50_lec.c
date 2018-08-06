b51eb3e71de15cac6d46c92ef70b4cf4b08b8a7a
mkirwa <mahlonkibiwott@gmail.com>
2018-08-05 19:49:01 -0500

    Add files via upload

--- s50_lec.c
+++ s50_lec.c
@@ -36,7 +36,7 @@ int main(void){
    string s = get_string("enter string: ");
 
 
-   for(int i=0; n=strlen(s); i<n; i++){
+   for(int i=0, n=strlen(s); i<n; i++){
 
       if(s[i]>='a'&& s[i]<='z'){
        printf("%c",s[i]-('a'-'A'));
