2e98a6341814fa0ae262203df1d97889b6fa1268
mkirwa <mahlonkibiwott@gmail.com>
2018-08-23 15:49:21 -0500

    Add files via upload

--- lecture3.c
+++ lecture3.c
@@ -1,31 +1,27 @@
-//A program to pick initials from a string
+//merge sort algorithm
 
 #include <stdio.h>
 #include <cs50.h>
 #include <ctype.h>
 #include <string.h>
 
+int sigma(int m);
 int main(void){
-
-    string name = get_string("name: ");
-    //creates a space for 4 characters, 3 characters from the user
-	//and the fourth character /0 that shows the end of the string
-    char initials[4];
-
-    int counter = 0;
-
-    for(int i = 0; i<strlen(name); i++){
-
-        if(isupper(name[i])){
-
-            initials [counter] = name[i];
-            counter++;
+    
+    int num;
+        do{
+            num = get_int("positive integer: ");
         }
-
-    }
-
-    initials[counter] = '\0';
-    printf("%s\n",initials);
+        while(n<1);
+        
+        int answer = sigma(num);
+        printf("%i", answer);
 
 }
 
+int sigma(int m){
+    int m=0;
+    for(int i=0; i<m; i++){
+        
+    }
+}
