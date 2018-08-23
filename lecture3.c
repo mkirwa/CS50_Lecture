20aaefe0c3c262eb2a3038fdbbb97c96ccabd5b8
mkirwa <mahlonkibiwott@gmail.com>
2018-08-23 12:08:39 -0500

    Add files via upload

--- lecture3.c
+++ lecture3.c
@@ -1,16 +1,31 @@
 //A program to pick initials from a string
-#include <cs50.h>
+
 #include <stdio.h>
+#include <cs50.h>
 #include <ctype.h>
 #include <string.h>
 
 int main(void){
 
-	string name = get_string("name: ");
-	//creates a space for 4 characters, 3 characters from the user 
+    string name = get_string("name: ");
+    //creates a space for 4 characters, 3 characters from the user
 	//and the fourth character /0 that shows the end of the string
-	char initals[4];
+    char initials[4];
 
-	counter = 0; 
+    int counter = 0;
+
+    for(int i = 0; i<strlen(name); i++){
+
+        if(isupper(name[i])){
+
+            initials [counter] = name[i];
+            counter++;
+        }
+
+    }
+
+    initials[counter] = '\0';
+    printf("%s\n",initials);
 
 }
+
